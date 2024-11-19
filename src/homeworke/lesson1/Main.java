4de62ee4344a79243191c58726d090460c1b8588
Vladislav <1974vlad0412@gmail.com>
2024-11-19 20:01:09 +0300

    ДЗ №1

---
+++ src/homeworke/lesson1/Main.java
@@ -0,0 +1,9 @@
+package homeworke.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        System.out.println();
+        Child child1 = new Child("Ivan");
+    }
+}
